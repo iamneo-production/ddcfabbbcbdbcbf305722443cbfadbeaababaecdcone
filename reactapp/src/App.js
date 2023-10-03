846fc3b15e51559e1feaa58393ed58c8be964c71
Iamneo <techadmin@iamneo.ai>
2023-10-03 13:42:59 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Quiz from "./quiz/Quiz";
 
 function App() {
   return (
     <div className="App">
-      
+      <Quiz />
     </div>
   );
 }
